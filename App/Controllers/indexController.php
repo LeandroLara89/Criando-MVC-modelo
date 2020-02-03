3f6a999b7bb59bdf242defde68b5f251e4fb5475
Leandro <leandrodsilvalara@yahoo.com.br>
2020-02-03 18:48:28 -0300

    Conexao com banco de dados Models

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -2,19 +2,37 @@
 
 namespace App\Controllers;
 
+//os recursos do miniframework
 use MF\Controller\Action;
+use MF\Model\Container;
+
+
+//os models
+use App\Models\Produto;
+use App\Models\Info;
+
 
 class IndexController extends Action {
 
 	public function index() {
 
-		$this->view->dados = array('Sofá', 'Cadeira', 'Cama');
+		$produto = Container::getModel('Produto');
+
+		$produtos = $produto->getProdutos();
+
+		@$this->view->dados = $produtos;
+
 		$this->render('index', 'layout1');
 	}
 
 	public function sobreNos() {
+
+		$info = Container::getModel('Info');
+
+		$informacoes = $info->getInfo();
 		
-		$this->view->dados = array('Notebook', 'Smartphone');
+		@$this->view->dados = $informacoes;
+
 		$this->render('sobreNos', 'layout1');
 	}
 
